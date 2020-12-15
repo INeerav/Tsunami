ec7b0aba5d1047b9eb8b62508891fcac08494917
Nirav Patel <Nirav.Patel@preqin.com>
2020-12-15 14:05:22 +0530

    Initial commit for the tsunami

---
+++ Tsunami/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tsunami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("URI please :");
+            string url = Console.ReadLine();
+            Console.WriteLine("Token please :");
+            string token = Console.ReadLine();
+            Console.WriteLine("Is Get?(Yes or No) :");
+            string isGetString = Console.ReadLine();
+            bool isGet = true;
+            StringContent content = new StringContent("");
+            if (!string.IsNullOrEmpty(isGetString) && isGetString.ToLower() != "yes") {
+                isGet = false;
+                Console.WriteLine("Body in json:");
+                string body = Console.ReadLine();
+                content = new StringContent(body, Encoding.UTF8, "application/json");
+            }
+            Console.WriteLine("Concurrent Requests : 50 , Interval Time : 5 seconds");
+            while (true)
+            {
+                Call(url, token, isGet, isGet ? null: content);
+                Thread.Sleep(5000);
+            }
+        }
+
+        static void Call(string url,string token,bool isGet = true, StringContent content = null)
+        {
+            TsunamiService randoService = new TsunamiService(url: url, token, maxConcurrentRequests: 100, isGet, content);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Run(async () =>
+                {
+                    Console.WriteLine($"Flooding with reqeusts!! ");
+                    Console.WriteLine(await randoService.GetResponse());
+                });
+            }           
+        }
+    }
+}
---
+++ Tsunami/Services/TsunamiService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class TsunamiService
+{
+	private readonly HttpClient _httpClient;
+	private readonly string GetRandomNumberUrl;
+	private SemaphoreSlim semaphore;
+	private long circuitStatus;
+	private const long OPEN = 0;
+	private const long TRIPPED = 1;
+	public string UNAVAILABLE = "Unavailable";
+	public bool _isGet;
+	public StringContent _httpContent;
+
+	public TsunamiService(string url,string token, int maxConcurrentRequests, bool isGet = true, StringContent content = null)
+	{
+		GetRandomNumberUrl = $"{url}";
+		_httpClient = new HttpClient();
+		_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);		
+		_isGet = isGet;
+		if (!isGet) {
+			_httpContent = content;
+		}
+		SetMaxConcurrency(url, maxConcurrentRequests);
+		semaphore = new SemaphoreSlim(maxConcurrentRequests);
+
+		circuitStatus = OPEN;
+	}
+
+	private void SetMaxConcurrency(string url, int maxConcurrentRequests)
+	{
+		ServicePointManager.FindServicePoint(new Uri(url)).ConnectionLimit = maxConcurrentRequests;
+	}
+
+	public void OpenCircuit()
+	{
+		if (Interlocked.CompareExchange(ref circuitStatus, OPEN, TRIPPED) == TRIPPED)
+		{
+			Console.WriteLine("Opened circuit");
+		}
+	}
+
+	private void TripCircuit(string reason)
+	{
+		if (Interlocked.CompareExchange(ref circuitStatus, TRIPPED, OPEN) == OPEN)
+		{
+			Console.WriteLine($"Tripping circuit because: {reason}");
+		}
+	}
+
+	private bool IsTripped()
+	{
+		return Interlocked.Read(ref circuitStatus) == TRIPPED;
+	}
+	public async Task<string> GetResponse()
+	{
+		try
+		{
+			await semaphore.WaitAsync();
+
+			if (IsTripped())
+			{
+				return UNAVAILABLE;
+			}
+			HttpResponseMessage response;
+
+			if (!_isGet)
+			{
+				 response = await _httpClient.PostAsync(GetRandomNumberUrl, _httpContent);
+			}
+			else {
+				response = await _httpClient.GetAsync(GetRandomNumberUrl);
+			}
+
+			if (response.StatusCode != HttpStatusCode.OK)
+			{
+				TripCircuit(reason: $"Status not OK. Status={response.StatusCode}");
+				return UNAVAILABLE;
+			}
+
+			return await response.Content.ReadAsStringAsync();
+		}
+		catch (Exception ex) when (ex is OperationCanceledException || ex is TaskCanceledException)
+		{
+			Console.WriteLine("Timed out");
+			TripCircuit(reason: $"Timed out");
+			return UNAVAILABLE;
+		}
+		finally
+		{
+			semaphore.Release();
+		}
+	}
+}
